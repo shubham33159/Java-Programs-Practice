63651a31ece1c7d86b927840237836fe5537f1fd
Shubham <shubham33159@gmail.com>
2023-07-25 20:54:22 +0530

    Modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,23 @@ class HelloWorld
     public static void main(String args[])
     {
         System.out.println("Hello World");
+
+        int  num1 = 100;
+        int num2 = 200;
+        System.out.println(num1 + num2);
+
+        // integer variable declaration
+
+        int a=5;
+        float f= 5.6f;
+        long b=600l; // e.g 578l, 100000l represent long integer type
+        byte c=127;
+        short s=128; 
+
+
+        int bin = 0b101;
+        System.out.println(bin);
+
+
     }
 }
